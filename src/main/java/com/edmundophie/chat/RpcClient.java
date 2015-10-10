1aad0d3b61f71f576ac4a080d873a6c1857839a2
Edmund Ophie <edmund.ophie@yahoo.com>
2015-10-10 14:37:17 +0700

    All features done but multiclient not checked yet

---
+++ src/main/java/com/edmundophie/chat/RpcClient.java
@@ -0,0 +1,298 @@
+package com.edmundophie.chat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.*;
+import com.rabbitmq.client.Channel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.UUID;
+
+/**
+ * Created by edmundophie on 10/9/15.
+ */
+public class RpcClient {
+    private static final String HOSTNAME = "localhost";
+    private static final String ENCODING = "UTF-8";
+    private static final String MESSAGE_EXCHANGE_NAME = "messages";
+
+    private Connection connection;
+    private Channel channel;
+    private Channel messageInChannel;
+    private String requestQueueName = "rpc_queue";
+    private String replyQueueName;
+    private QueueingConsumer consumer;
+    private Consumer messageInConsumer;
+
+    private ObjectMapper mapper;
+    private boolean isLoggedIn;
+    private String nickname;
+
+    public RpcClient() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(HOSTNAME);
+        factory.setConnectionTimeout(0);
+        Connection connection = factory.newConnection();
+        channel = connection.createChannel();
+
+        replyQueueName = channel.queueDeclare().getQueue();
+        consumer = new QueueingConsumer(channel);
+        channel.basicConsume(replyQueueName, true, consumer);
+
+        messageInChannel = connection.createChannel();
+        messageInChannel.exchangeDeclare(MESSAGE_EXCHANGE_NAME, "direct");
+
+        messageInConsumer = new DefaultConsumer(messageInChannel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(message);
+            }
+        };
+
+        mapper = new ObjectMapper();
+        isLoggedIn = false;
+        nickname = "";
+    }
+
+    public String call(String message) throws Exception {
+        String response = null;
+        String corrId = UUID.randomUUID().toString();
+
+        AMQP.BasicProperties props = new AMQP.BasicProperties
+                .Builder()
+                .correlationId(corrId)
+                .replyTo(replyQueueName)
+                .build();
+        channel.basicPublish("", requestQueueName, props, message.getBytes(ENCODING));
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            if(delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response = new String(delivery.getBody(), ENCODING);
+                break;
+            }
+        }
+
+        return response;
+    }
+
+    public void close() throws Exception {
+        channel.close();
+        connection.close();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("* Starting client...");
+
+        RpcClient rpcClient = null;
+        String response = null;
+
+        try {
+            rpcClient = new RpcClient();
+
+            rpcClient.perform();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(rpcClient!=null) {
+                try{
+                    rpcClient.close();
+                } catch (Exception ignore) {}
+            }
+        }
+        System.exit(0);
+    }
+
+    private static void printInvalidCommand() {
+        System.err.println("* Invalid Command");
+    }
+
+    private void perform() throws Exception {
+        System.out.println("* Client started");
+
+        String command = null;
+        do {
+//            String consoleNickname = (!isLoggedIn)?"$ ":nickname+"$ ";
+//            System.out.print(consoleNickname);
+            String input = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
+
+            if(input.isEmpty())
+                printInvalidCommand();
+            else {
+                String parameter = "";
+                int i = input.indexOf(" ");
+
+                if (i > -1) {
+                    command = input.substring(0, i);
+                    parameter = input.substring(i + 1);
+                } else
+                    command = input;
+
+                if (command.equalsIgnoreCase("NICK")) {
+                    login(command.toUpperCase(), parameter);
+                } else if (command.equalsIgnoreCase("JOIN")) {
+                    join(command.toUpperCase(), parameter);
+                } else if (command.equalsIgnoreCase("LEAVE")) {
+                    leave(command.toUpperCase(), parameter);
+                } else if (command.equalsIgnoreCase("LOGOUT")) {
+                    logout(command.toUpperCase());
+                } else if (command.equalsIgnoreCase("EXIT")) {
+                    exit(command.toUpperCase());
+                } else if (command.charAt(0) == '@') {
+                    sendMessage(command.substring(1), parameter);
+                } else {
+                    broadcastMessage(input);
+                }
+            }
+        } while (!command.equalsIgnoreCase("EXIT"));
+    }
+
+    private void login(String command, String parameter) throws Exception {
+        if(isLoggedIn) {
+            System.err.println("* Please logout first!");
+        } else {
+            Request request = new Request();
+            request.setCommand(command);
+            request.setNickname(parameter);
+
+            String responseJson = call(request.toString());
+            Response response = mapper.readValue(responseJson, Response.class);
+
+            if(response.isStatus()) {
+                nickname = response.getNickname();
+                isLoggedIn = true;
+                System.out.println(response.getMessage());
+
+//                Runnable messageListener = new Runnable() {
+//                    public void run() {
+//                        System.out.println("* Message listener started...");
+//                        while (isLoggedIn) {
+//                            List<FetchMessagesResponse.Message> messages = getMessages();
+//                            if (messages != null && !messages.isEmpty()) {
+//                                lastReceivedMessageTimestamp = messages.get(messages.size() - 1).getTimestamp();
+//                                for(FetchMessagesResponse.Message msg:messages)
+//                                    System.out.println(msg.getText());
+//                            }
+//                        }
+//                    }
+//                };
+//
+//                messageListenerThread = new Thread(messageListener);
+//                messageListenerThread.start();
+            } else {
+                System.err.println(response.getMessage());
+            }
+        }
+    }
+
+    private void join(String command, String parameter) throws Exception {
+        if(!isLoggedIn) System.err.println("* Please login first!");
+        else if(parameter==null || parameter.isEmpty()) printInvalidCommand();
+        else {
+            Request request = new Request();
+            request.setCommand(command);
+            request.setChannelName(parameter);
+            request.setNickname(nickname);
+
+            String responseJson = call(request.toString());
+            Response response = mapper.readValue(responseJson, Response.class);
+
+            if(response.isStatus()) {
+                messageInChannel.queueDeclare(parameter, false, false, true, null); //channel.queueDeclare().getQueue();
+                messageInChannel.queueBind(parameter, MESSAGE_EXCHANGE_NAME, parameter);
+                messageInChannel.basicConsume(parameter, true, messageInConsumer);
+                System.out.println(response.getMessage());
+            } else
+                System.err.println(response.getMessage());
+        }
+    }
+
+    private void leave(String command, String parameter) throws Exception {
+        if(!isLoggedIn) System.err.println("* Please login first!");
+        else if(parameter==null || parameter.isEmpty()) printInvalidCommand();
+        else  {
+            Request request = new Request();
+            request.setCommand(command);
+            request.setChannelName(parameter);
+            request.setNickname(nickname);
+
+            String responseJson = call(request.toString());
+            Response response = mapper.readValue(responseJson, Response.class);
+
+            if(response.isStatus()) {
+                messageInChannel.queueUnbind(parameter, MESSAGE_EXCHANGE_NAME, parameter);
+//                messageInChannel.queueDelete(parameter);
+                System.out.println(response.getMessage());
+            } else
+                System.err.println(response.getMessage());
+        }
+    }
+
+    private void logout(String command) throws Exception {
+        if(!isLoggedIn) System.err.println("* Please login first!");
+        else {
+            Request request = new Request();
+            request.setCommand(command);
+            request.setNickname(nickname);
+
+            String responseJson = call(request.toString());
+            Response response = mapper.readValue(responseJson, Response.class);
+
+            if(response.isStatus()) {
+                isLoggedIn = false;
+                nickname = "";
+                System.out.println(response.getMessage());
+            } else {
+                System.err.println(response.getMessage());
+            }
+        }
+    }
+
+    private void exit(String command) throws Exception {
+        if(isLoggedIn)
+            logout(command);
+
+        System.out.println("* Program exited");
+    }
+
+
+    private void sendMessage(String channelName, String message) throws Exception {
+        if(!isLoggedIn) System.err.println("* Please login first!");
+        else if(message==null || message.isEmpty()) printInvalidCommand();
+        else {
+            Request request = new Request();
+            request.setCommand("SEND");
+            request.setChannelName(channelName);
+            request.setMessage(message);
+            request.setNickname(nickname);
+
+            String responseJson = call(request.toString());
+            Response response = mapper.readValue(responseJson, Response.class);
+
+            if(!response.isStatus()) {
+                System.err.println(response.getMessage());
+            }
+        }
+    }
+
+    private void broadcastMessage(String message) throws Exception {
+        if(!isLoggedIn) System.err.println("Please login first!");
+        else {
+            Request request = new Request();
+            request.setCommand("BROADCAST");
+            request.setMessage(message);
+            request.setNickname(nickname);
+
+            String responseJson = call(request.toString());
+            Response response = mapper.readValue(responseJson, Response.class);
+
+            if(!response.isStatus()) {
+                System.err.println(response.getMessage());
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/edmundophie/chat/RpcServer.java
@@ -0,0 +1,271 @@
+package com.edmundophie.chat;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * Created by edmundophie on 10/9/15.
+ */
+public class RpcServer {
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
+    private static final String HOSTNAME = "localhost";
+    private static final String ENCODING = "UTF-8";
+    private static final String MESSAGE_EXCHANGE_NAME = "messages";
+    private static final int MAX_GENERATED_RANDOM_ACCOUNT_INT = 99999;
+    private static Map<String, User> userMap;
+    private static Map<String, Channel> channelMap;
+    private static Map<String, List<Message>> messageListMap; // TODO HIGH change to publish subscribe
+    private static com.rabbitmq.client.Channel messageOutChannel;
+
+    public static void main (String[] args) {
+        Connection connection = null;
+        com.rabbitmq.client.Channel channel = null;
+
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost(HOSTNAME);
+
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+
+            channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+            channel.queuePurge(RPC_QUEUE_NAME);
+            channel.basicQos(1);
+
+            QueueingConsumer consumer = new QueueingConsumer(channel);
+            channel.basicConsume(RPC_QUEUE_NAME, false, consumer);
+
+            messageOutChannel = connection.createChannel();
+            messageOutChannel.exchangeDeclare(MESSAGE_EXCHANGE_NAME, "direct");
+
+            userMap =  new HashMap<String, User>();
+            channelMap =  new HashMap<String, Channel>();
+            messageListMap = new HashMap<String, List<Message>>();
+
+            System.out.println(" - RPC server started");
+
+            while (true) {
+                String response = null;
+
+                QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+
+                AMQP.BasicProperties props = delivery.getProperties();
+                AMQP.BasicProperties replyProps = new AMQP.BasicProperties
+                        .Builder()
+                        .correlationId(props.getCorrelationId())
+                        .build();
+
+                String message = new String(delivery.getBody(), ENCODING);
+
+                response = "" + processMessage(message);
+
+                channel.basicPublish("", props.getReplyTo(), replyProps, response.getBytes(ENCODING));
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String processMessage(String message) throws JsonProcessingException {
+        Request request = null;
+        try {
+            request = new ObjectMapper().readValue(message, Request.class);
+        } catch (IOException e) {
+            Response response = new Response();
+            response.putStatus(false);
+            response.setMessage("* Server Encountered An Error On Processing Message!");
+            return new ObjectMapper().writeValueAsString(response);
+        }
+
+        if (request.getCommand().equalsIgnoreCase("NICK")) {
+            return login(request.getNickname());
+        } else if (request.getCommand().equalsIgnoreCase("JOIN")) {
+            return join(request.getNickname(), request.getChannelName());
+        } else if (request.getCommand().equalsIgnoreCase("LEAVE")) {
+            return leave(request.getNickname(), request.getChannelName());
+        } else if (request.getCommand().equalsIgnoreCase("LOGOUT")) {
+            return logout(request.getNickname());
+        } else if (request.getCommand().equalsIgnoreCase("EXIT")) {
+            return exit(request.getNickname());
+        } else if (request.getCommand().equalsIgnoreCase("SEND")) {
+            return sendMessage(request.getNickname(), request.getChannelName(), request.getMessage());
+        } else if (request.getCommand().equalsIgnoreCase("BROADCAST")) {
+            return broadcastMessage(request.getNickname(), request.getMessage());
+        } else {
+            Response response = new Response();
+            response.putStatus(false);
+            response.setMessage("* Unknown Message Command!");
+            return new ObjectMapper().writeValueAsString(response);
+        }
+    }
+
+    private static String login(String nickname) {
+        System.out.println(" - Login method invoked");
+        StringBuilder message = new StringBuilder();
+
+        if(nickname==null || nickname.isEmpty() || userMap.containsKey(nickname)) {
+            if(userMap.containsKey(nickname)) message.append("* Username exist!\n");
+            nickname = generateRandomNickname();
+            message.append("* Random user generated\n");
+        }
+        message.append("* Successfully logged in as " + nickname);
+
+        userMap.put(nickname, new User(nickname));
+
+        Response response = new Response(true, message.toString(), nickname);
+        return response.toString();
+    }
+
+    private static String generateRandomNickname() {
+        String newNickname;
+        Random random = new Random();
+        do {
+            newNickname = "user" + random.nextInt(MAX_GENERATED_RANDOM_ACCOUNT_INT);
+        } while(userMap.containsKey(newNickname));
+
+        return newNickname;
+    }
+
+    public static String join(String nickname, String channelName) {
+        System.out.println(" - " + nickname + " requested to join #" + channelName);
+
+        List userChannelList = userMap.get(nickname).getJoinedChannel();
+        StringBuilder message = new StringBuilder();
+        Response response = new Response();
+
+        if(userChannelList.contains(channelName)) {
+            message.append("* You are already a member of #" + channelName);
+            response.putStatus(false);
+        } else {
+            if(!channelMap.containsKey(channelName)) {
+                channelMap.put(channelName, new Channel(channelName));
+                messageListMap.put(channelName, new ArrayList<Message>());
+                message.append("* Created new channel #" + channelName + "\n");
+            }
+
+            userChannelList.add(channelName);
+            message.append("* #" + channelName + " joined successfully");
+            response.putStatus(true);
+        }
+
+        response.setMessage(message.toString());
+        return response.toString();
+    }
+
+    public static String leave(String nickname, String channelName) {
+        System.out.println("- " + nickname + " request to leave #" + channelName);
+
+        StringBuilder message = new StringBuilder();
+        Response response = new Response();
+
+        if(!userMap.get(nickname).getJoinedChannel().contains(channelName)) {
+            System.err.println("- Failed to leave channel. " + nickname + " is not a member of #" + channelName);
+            message.append("* Failed to leave.\n* You are not a member of #" + channelName);
+            response.putStatus(false);
+        } else {
+            userMap.get(nickname).getJoinedChannel().remove(channelName);
+            response.putStatus(true);
+            message.append("* You are no longer a member of #" + channelName);
+        }
+
+        response.setMessage(message.toString());
+        return response.toString();
+    }
+
+    public static String logout(String nickname) {
+        System.out.println(" - " + nickname + " requested to logout");
+        userMap.remove(nickname);
+
+        Response response = new Response();
+        response.putStatus(true);
+        response.setMessage("* " + nickname + " have been logged out");
+
+        return response.toString();
+    }
+
+    public static String exit(String nickname) {
+        return logout(nickname);
+    }
+
+    public static String sendMessage(String nickname, String channelName, String message) {
+        System.out.println("- " + nickname + " sends a message to #" + channelName);
+        StringBuilder returnedMessage = new StringBuilder();
+        Response response = new Response();
+
+        List<String> userChannelList = userMap.get(nickname).getJoinedChannel();
+        if(!userChannelList.contains(channelName)) {
+            System.err.println("- Failed to send " + nickname + " message to #" + channelName + ". User is not a member of the channel.");
+            returnedMessage.append("* You are not a member of #" + channelName);
+            response.putStatus(false);
+        } else {
+//            channelMap.get(channelName).getMessages().add(msg); // TODO LOW is channelMap really needed?
+//            msg.setText("@" + channelName + " " + nickname + ": " + msg.getText());
+//            messageListMap.get(channelName).add(msg);
+            try {
+                Message msg = new Message(nickname, message);
+                distributeMessage(msg, channelName);
+            } catch (IOException e) {
+                e.printStackTrace();
+                response.putStatus(false);
+                response.setMessage("* Server Encountered An Error On Publishing the Message");
+                return response.toString();
+            }
+            response.putStatus(true);
+        }
+
+        response.setMessage(returnedMessage.toString());
+        return response.toString();
+    }
+
+    public static String broadcastMessage(String nickname, String message) {
+        System.out.println("- " + nickname + " broadcasts a message");
+        StringBuilder returnedMessage = new StringBuilder();
+        Response response = new Response();
+
+        List<String> userChannelList = userMap.get(nickname).getJoinedChannel();
+        if(userChannelList.size()==0) {
+            System.err.println("- Failed to send " + nickname + " message. No channel found.");
+            returnedMessage.append("* Failed to send the message\n* You haven't join any channel yet");
+            response.putStatus(false);
+        } else {
+//            for(String channelName:userChannelList) { // TODO LOW async
+//                channelMap.get(channelName).getMessages().add(msg);
+//                Message temp = new Message(nickname, "@" + channelName + " " + nickname + ": " + msg.getText());
+//                messageListMap.get(channelName).add(temp);
+//            }
+            try {
+                Message msg = new Message(nickname, message);
+                distributeMessage(msg, userChannelList);
+            } catch (IOException e) {
+                e.printStackTrace();
+                response.putStatus(false);
+                response.setMessage("* Server Encountered An Error On Publishing the Message");
+                return response.toString();
+            }
+            response.putStatus(true);
+        }
+
+        response.setMessage(returnedMessage.toString());
+        return response.toString();
+    }
+
+    public static void distributeMessage(Message message, List<String> userChannelList) throws IOException {
+        for(String channelName:userChannelList) {
+           String enrichedMessage = "@" + channelName + " " + message.getSender()+ ": " + message.getText();
+            messageOutChannel.basicPublish(MESSAGE_EXCHANGE_NAME, channelName, null, enrichedMessage.getBytes());
+        }
+    }
+
+    public static void distributeMessage(Message message, String channelName) throws IOException {
+        String enrichedMessage = "@" + channelName + " " + message.getSender()+ ": " + message.getText();
+        messageOutChannel.basicPublish(MESSAGE_EXCHANGE_NAME, channelName, null, enrichedMessage.getBytes());
+    }
+}
